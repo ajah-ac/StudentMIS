7c6d144f454406ada6b31c0e9080ef0c911aa058
ajah <ajahajang00@gmail.com>
2026-04-24 16:03:54 +0200

    Style navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,12 @@
-import {Link} from 'react-router-dom'
+import { NavLink} from 'react-router-dom'
 import {Sun} from 'lucide-react'
 export const Navbar = () => {
   return (
-    <div>
+    <div className=' shadow-xl py-4 px-10 flex align-center justify-between'>
 <h1>StudentMis</h1>
-<nav>
-    <Link to='/'>Register</Link>
-    <Link to='/students'>Students</Link>
+<nav className='flex justify-evenly gap-4 '>
+    <NavLink to='/'>Register</NavLink>
+    <NavLink to='/students'>Students</NavLink>
     <button><Sun/></button>
 </nav>
 
