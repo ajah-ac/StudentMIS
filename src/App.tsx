fc70cf2a5db025a6d234d9d5b3f4b8e3144de76e
ajah <ajahajang00@gmail.com>
2026-04-24 15:57:39 +0200

    Add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
 import { useState } from 'react'
+import Layout from './components/Layout'
+import { createBrowserRouter,RouterProvider } from 'react-router'
+import Register from './pages/Register'
+import Students from './pages/Students'
+import Confirmation from './pages/Confirmation'
+import Course from './pages/Course'
 
+const router=createBrowserRouter([
+  {path:'/',
+    element:<Layout/>,
+    children:[
+      {
+        index:true,
+        element:<Register/>
+      },
+      {
+        path:'/students',
+        element:<Students/>
+      },
+      {
+        path:'/confirmation',
+        element:<Confirmation/>
+      },
+      {
+        path:'/course',
+        element:<Course/>
+      }
+    ]
+  }
+])
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-green-200'>Hello</h1>
+    <RouterProvider router={router}/>
+   
     </>
   )
 }
