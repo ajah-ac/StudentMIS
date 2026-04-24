71e8f7709668bacda391b0079dc97d5d4c7bd1a7
ajah <ajahajang00@gmail.com>
2026-04-24 16:17:42 +0200

    create custom Theme Context hook

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,23 @@
+import { useState ,createContext, useContext} from "react"
+
+interface Theme{
+theme:string,
+toggleTheme:()=>void
+}
+const themeContext=createContext<Theme|null>(null)
+
+const ThemeProvider = ({children}:{children:React.ReactNode}) => {
+    const [theme,setTheme]=useState<'light' |'dark'>('light')
+const toggleTheme=()=>{setTheme(prev=>prev!=='light'?'dark':'light')}
+  return (
+<themeContext.Provider value={{theme,toggleTheme}}>{children}</themeContext.Provider>
+  )
+}
+
+export default ThemeProvider
+const useThemeContext=()=>{
+    const context=useContext(themeContext)
+    if(!context){
+throw new Error('useThemeContext must be used inside ThemeProvider')    }
+return context
+}
