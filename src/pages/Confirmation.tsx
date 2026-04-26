3d19032f1bf6d1f516843800e430a1848c7c8667
ajah <ajahajang00@gmail.com>
2026-04-26 12:14:16 +0200

    Register and add Student to student List

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,7 +1,69 @@
-
+import { useEffect, useState } from "react"
+import { useLocation, useNavigate } from "react-router"
+type user={
+  fullName:string,
+  email:string,
+  phone:string,
+  course:string
+}
 const Confirmation = () => {
+  const location=useLocation()
+  const data=location.state;
+  const navigate=useNavigate()
+  const [student,setStudent]=useState<user| null>(null)
+  const [studentList,setStudentList]=useState<user[]>([])
+
+ useEffect(()=>{
+  if(location.state){
+    setStudent(location.state)
+  }
+  else{
+const stored=localStorage.getItem('studentForm')
+ if(stored){
+    setStudent(JSON.parse(stored))
+  }
+  }
+const storedList=localStorage.getItem('studentList')
+ 
+  if(storedList && storedList!=='undefined'){
+      setStudentList(JSON.parse(storedList))}
+
+ },[location.state])
+ useEffect(()=>{
+  
+  localStorage.setItem('studentList',JSON.stringify(studentList))
+ },[studentList])
+ if(!student) return <p> No student to reguster</p>
+ function onRegister(){
+  if(!student) return 
+
+  const updatedList=[...studentList,student]
+setStudentList(updatedList)
+alert(`Student ${student.fullName} registered successfully`)
+navigate('/students',{state:data,replace:true})
+ }
   return (
-    <div>Confirmation</div>
+    <div>
+      
+<div className="flex  flex-col justify-center sm:p-4 p-10 gap-4  text-md sm:text-2xl items-center ">
+  <h1>Confirm Full Details</h1>
+  <div className="flex flex-col gap-3 py-4">
+  <h2>Full Name: {student.fullName}</h2>
+<h2>Email Address: {student.email}</h2>
+<h2>Phone: {student.phone}</h2>
+<h2>Course: {student.course}</h2>
+
+</div>
+<div className="flex  justify-between gap-4 w-1/3 ">
+<button 
+onClick={()=>{navigate('/',{state:data})}}
+className="border w-50 py-1 rounded-2xl"
+>Edit</button>
+<button className="border w-50 py-1 rounded-2xl"
+onClick={onRegister}
+> Register</button></div>
+</div>
+</div>
   )
 }
 
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,15 +1,68 @@
-
+import { useLocation, useNavigate } from "react-router-dom"
+import { useEffect, useState } from "react"
 const Register = () => {
-  return (
-<>
-<form action="" className="">
-<input type="text"  placeholder="Enter Student Name"/>
-<input type="email" name="" id=""  placeholder="Enter Email Address"/>
-<input type="number" name="" id="" placeholder="Enter Phone number" />
-<button type='button'>Review </button>
-</form>
+      const navigate = useNavigate();
+      const location=useLocation()
+  const [form, setForm] = useState({
+    fullName: '',
+    email: '',
+    phone: '',
+    course: ''
+  })
+  useEffect(()=>{localStorage.setItem('studentForm',JSON.stringify(form))},[form])
+  function handleChange(e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) {
+    e.preventDefault()
+    const { name, value } = e.target;
+    setForm(prev => ({ ...prev, [name]: value }))
 
-</>  )
+  }
+  useEffect(()=>{
+      if (location.state) {
+    setForm(location.state);
+  }
+  },[location.state])
+  function handleSubmit() {
+    
+    navigate('/confirmation',{state:form})
+  }
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+
+      <form onSubmit={(e)=>e.preventDefault()} className="border rounded-2xl flex flex-col px-20 py-20 max-w-xl gap-4">
+        <input
+          name='fullName'
+          type="text"
+          value={form.fullName}
+          onChange={handleChange}
+          required
+          placeholder="Enter Student Name" />
+
+        <input
+          name='email'
+          type="text"
+          value={form.email}
+          onChange={handleChange}
+          required id="" placeholder="Enter Email Address" />
+        <input
+          value={form.phone}
+          onChange={handleChange}
+          required name="phone"
+          placeholder="Enter Phone number" />
+
+        <select name="course"
+          value={form.course}
+          onChange={handleChange}
+          id="">
+          <option value="" disabled selected>Select Course</option>
+          <option value="math">Math</option>
+          <option value="science">Science</option>
+          <option value="english">English</option>
+          <option value="history">History</option>
+        </select>
+        <button type='submit' onClick={handleSubmit}>Review </button>
+      </form>
+
+    </div>)
 }
 
 export default Register
