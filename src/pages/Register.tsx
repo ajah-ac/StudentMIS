bde7af85d1549ed063ea4b1cfe5e7a82caa96b8e
ajah <ajahajang00@gmail.com>
2026-04-27 11:40:42 +0200

    Validate Form

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -3,17 +3,45 @@ import { useEffect, useState } from "react"
 const Register = () => {
   const navigate = useNavigate();
   const location = useLocation()
+  const [errors,setError]=useState ({fullName: '',
+    email: '',
+    phone: '',
+    course: ''})
   const [form, setForm] = useState({
     fullName: '',
     email: '',
     phone: '',
     course: ''
   })
-  useEffect(() => { localStorage.setItem('studentForm', JSON.stringify(form)) }, [form])
+useEffect(() => {
+  const stored = localStorage.getItem('studentForm')
+  if (stored) {
+    setForm(JSON.parse(stored))
+  }
+}, [])
   function handleChange(e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) {
     e.preventDefault()
     const { name, value } = e.target;
-    setForm(prev => ({ ...prev, [name]: value }))
+      if (name === 'phone' && !/^\d*$/.test(value)) return;
+        setForm(prev => ({ ...prev, [name]: value }))
+
+let error=''
+
+if(name=='fullName' && value.trim()===''){
+  error='Full name is required'
+}
+if(name==='course'){
+  if(value.trim()==='')
+    error='Please select a course '
+}
+
+if(name==='phone' && value.trim().length<10){
+  error='Phone must be at least 10 digits'
+}
+if(name==='email' && !value.includes('@')){
+  error='Enter a valid email'
+}
+setError(prev=>({...prev,[name]:error}))
 
   }
   useEffect(() => {
@@ -22,22 +50,20 @@ const Register = () => {
     }
   }, [location.state])
 
-  function handleSubmit() {
-
+  function handleSubmit(e:React.SubmitEvent) {
+e.preventDefault()
     navigate('/confirmation', { state: form, replace: true })
   }
-  const isFormValid= form.fullName.trim()!==' '&&
-  form.fullName.trim()!==''&&
-  form.email.trim()!==''&&
-  form.phone.trim()!==''&&
-  form.course.trim()!=='';
-
+  const isFormValid= 
+  Object.values(form).every(val=>val.trim()!=='') &&
+Object.values(errors).every(err=>err==='')
+   
   return (
-    <div className="min-h-screen flex items-center max-w-6xl justify-center overflow-x-hidden">
+    <div className="min-h-screen flex items-center max-w-6xl justify-center m-auto">
 
-      <form onSubmit={handleSubmit} className="border  rounded-2xl flex flex-col px-10 py-20 max-w-xl gap-4">
+      <form onSubmit={handleSubmit} className=" shadow-2xl border  rounded-2xl flex flex-col px-10 py-20 max-w-xl gap-4">
         <input
-          className="  text-red-400
+          className="  
           outline-0 rounded border-b px-8 py-3 "
           name='fullName'
           type="text"
@@ -45,21 +71,31 @@ const Register = () => {
           onChange={handleChange}
           required
           placeholder="Enter Student Name" />
+          {form.fullName && errors.fullName && (
+  <p className="text-red-500 text-sm">{errors.fullName}</p>
+)}
 
         <input
           className=" outline-0 rounded border-b px-8 py-3 "
           name='email'
           type="text"
-
           value={form.email}
           onChange={handleChange}
           required id="" placeholder="Enter Email Address" />
+          { form.email && errors.email && (
+  <p className="text-red-500 text-sm">{errors.email}</p>
+)}
         <input
           className=" rounded border-b px-8 py-3 "
           value={form.phone}
           onChange={handleChange}
           required name="phone"
+          inputMode="numeric" 
+  pattern="[0-9]*"
           placeholder="Enter Phone number" />
+          { form.phone && errors.phone && (
+  <p className="text-red-500 text-sm">{errors.phone}</p>
+)}
 
         <select name="course"
           value={form.course}
@@ -73,6 +109,9 @@ className="dark:bg-gray-900"
           <option value="english">English</option>
           <option value="history">History</option>
         </select>
+        { form.course==='' && errors.course && (
+  <p className="text-red-500 text-sm">{errors.course}</p>
+)}
         <button type='submit'
         disabled={!isFormValid}
           className={` py-3 rounded-3xl text-black  ${isFormValid?' bg-blue-200 text-blue-900 dark:text-white text-black font-bold  dark:bg-gray-800':' dark:text-gray-700 text-gray-400 font-extralight dark:bg-gray-800 cursor-not-allowed'}`}
